b3c426ed3bbf187e70726e1a5a3f01b8e4437758
dangrt5 <randy.dang05@gmail.com>
2019-03-12 13:39:24 -0700

    Added dom creation and functions

--- main.js
+++ main.js
@@ -4,14 +4,28 @@ window.onload = () => {
 
 function init() {
     domCreate();
+    assignRandomPokeball();
+    applyHandlers();
 }
 
 function domCreate() {
-    for (let i = 0; i < 7; i++) {
+    for (let i = 0; i < 4; i++) {
         const pokeball = document.createElement("img");
+        pokeball.classList.add("pokeball");
         pokeball.src = "images/pokeball.png";
         document.querySelector('.pokeballs .col').appendChild(pokeball);
 
     }
+}
+
+function assignRandomPokeball() {
+
+}
+
+function applyHandlers() {
+    const pokeballs = document.querySelectorAll(".pokeball");
+    pokeballs.forEach(pokeball => {
+        
+    })
 }
 
